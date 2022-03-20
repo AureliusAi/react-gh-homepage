88cd4692d98f5a54d64e434291c8e792f6a6ba6e
AureliusAi <markus.aurelius.ai@gmail.com>
2022-03-21 01:11:02 +0900

    updated top page with some details to begin with

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/aurelius_ai_128_lightning_icon.png";
+import Box from "@mui/material/Box";
+import "./App.css";
+import Copyright from "./components/Copyright";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Box
+          sx={{
+            mb: 0,
+            pb: 0,
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
+          <Box mr={2}>
+            <img src={logo} className="App-logo" alt="Aurelius Ai Logo" />
+          </Box>
+          <h1>Aurelius Ai</h1>
+        </Box>
+        <Box sx={{ mt: 1 }} id="slogan">
+          Innovation in Web3 and AI
+        </Box>
       </header>
+      <Box px={2}>
+        <h2>Freelance Full-Stack DAPP/Web3 Developer located in Tokyo</h2>
+        <p>Services include</p>
+        <ul>
+          <li>Javascript/Typescripot/Front End</li>
+          <li>Solidity/Smart Contracts</li>
+          <li>Etherium/Cardano</li>
+          <li>NFT</li>
+          <li>DAO</li>
+          <li>ML/AI model developement in python for API backend</li>
+        </ul>
+        <Box>
+          contact: <a href="mailto:markus.aurelius.ai@gmail.com">email contact</a>
+        </Box>
+        <Box>
+          github: <a href="https://github.com/AureliusAi">https://github.com/AureliusAi</a>
+        </Box>
+        <Box>
+          twitter: <a href="https://twitter.com/AiAurelius">@AiAurelius</a>
+        </Box>
+      </Box>
+      <Copyright sx={{ pt: 4 }} />
     </div>
   );
 }
